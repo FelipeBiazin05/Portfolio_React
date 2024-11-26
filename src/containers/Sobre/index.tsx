69bbff2f33626c0c002e0960a183b00b2ddf59f5
FelipeBiazin05 <142363711+FelipeBiazin05@users.noreply.github.com>
2024-11-26 20:24:11 -0300

    Construindo a estrutura e a seção "Sobre mim"

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,15 +1,20 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
 
 const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
-    <Paragrafo>
+    <Paragrafo tipo="principal">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate hic
       blanditiis nam ratione praesentium aperiam doloribus, voluptatum velit
       culpa reprehenderit, nesciunt possimus, eligendi odio amet ipsa quibusdam
       iusto inventore? Id.
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=FelipeBiazin05&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=FelipeBiazin05&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
