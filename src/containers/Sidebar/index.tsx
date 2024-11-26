3ac2fac5a8e9c3f1feead4844001f2d0f30ee1e3
Felipe <felipegabrielsilvabiazin@gmail.com>
2024-11-26 13:28:56 -0300

    Começando a estilizar o site

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,13 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img
+      src="https://github.com/FelipeBiazin05.png"
+      alt="Minha foto de perfil no github"
+    />
+    <Titulo fontSize={20}>Felipe Biazin</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,16 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptate hic
+      blanditiis nam ratione praesentium aperiam doloribus, voluptatum velit
+      culpa reprehenderit, nesciunt possimus, eligendi odio amet ipsa quibusdam
+      iusto inventore? Id.
+    </Paragrafo>
+  </section>
+)
+
+export default Sobre
