f9f1a4df8286f171e4e4e92b2843ed241671a0e6
Felipe <felipegabrielsilvabiazin@gmail.com>
2024-11-27 13:18:10 -0300

    Feito dia 27-11-2024

--- src/styles.ts
+++ src/styles.ts
@@ -1,16 +1,19 @@
 import styled, { createGlobalStyle } from 'styled-components'
 
+import { Theme } from './Themes/dark'
+
 const EstiloGlobal = createGlobalStyle`
 * {
   margin: 0;
   padding: 0;
   font-family: "Inter", sans-serif;
   list-style: none;
-}
+}W
 
 body {
   padding-top: 80px;
   padding-bottom: 80px;
+  background-color: ${(props) => (props.theme as Theme).corDeFundo};
 
   @media (max-width: 768px) {
   padding-top: 16px;
